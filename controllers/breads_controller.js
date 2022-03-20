62b27a3d994d8fc45dde53cbc18a5a73018d6fc6
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-03-20 10:01:26 -0700

    Connected to database with helper functions

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -43,27 +43,32 @@ breads.post("/", (req, res) => {
 });
   
 // UPDATE
-breads.put('/:arrayIndex', (req, res) => {
-  if(req.body.hasGluten === 'on'){
-    req.body.hasGluten = true
+breads.put("/:id", (req, res) => {
+  if (req.body.hasGluten === "on") {
+    req.body.hasGluten = true;
   } else {
-    req.body.hasGluten = false
+    req.body.hasGluten = false;
   }
-  Bread[req.params.arrayIndex] = req.body
-  res.redirect(`/breads/${req.params.arrayIndex}`)
-})
+  Bread.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(
+    (updatedBread) => {
+      console.log(updatedBread);
+      res.redirect(`/breads/${req.params.id}`);
+    }
+  );
+});
 // EDIT
-breads.get('/:indexArray/edit', (req, res) => {
-    res.render('edit', {
-      bread: Bread[req.params.indexArray],
-      index: req.params.indexArray
-    })
-})
-
-// DELETE
-breads.delete("/:indexArray", (req, res) => {
-  Bread.splice(req.params.indexArray, 1);
-  res.status(303).redirect("/breads");
+breads.get("/:id/edit", (req, res) => {
+  Bread.findById(req.params.id).then((foundBread) => {
+    res.render("edit", {
+      bread: foundBread,
+    });
+  });
 });
 
+// DELETE
+breads.delete("/:id", (req, res) => {
+  Bread.findByIdAndDelete(req.params.id).then((deletedBread) => {
+    res.status(303).redirect("/breads");
+  });
+});
 module.exports = breads;
