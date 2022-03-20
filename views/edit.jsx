64b9ba57774e64c6e12e29c59a7ac01573e6bf72
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-03-20 10:51:29 -0700

    added form validation on the backend with baker options

--- views/edit.jsx
+++ views/edit.jsx
@@ -24,6 +24,15 @@ function Edit({ bread, index }) {
           defaultChecked={bread.hasGluten}
         />
         <br />
+        <label htmlFor="baker">Baker</label>
+        <select name="baker" id="baker" defaultValue={bread.baker}>
+          <option value="Rachel">Rachel</option>
+          <option value="Monica">Monica</option>
+          <option value="Joey">Joey</option>
+          <option value="Chandler">Chandler</option>
+          <option value="Ross">Ross</option>
+          <option value="Phoebe">Phoebe</option>
+        </select>
         <input type="submit" />
       </form>
     </Default>
--- views/new.jsx
+++ views/new.jsx
@@ -19,6 +19,15 @@ function New() {
         <label htmlFor="hasGluten">Has Gluten?</label>
         <input type="checkbox" name="hasGluten" id="hasGluten" defaultChecked />
         <br />
+        <label htmlFor="baker">Baker</label>
+        <select name="baker" id="baker">
+          <option value="Rachel">Rachel</option>
+          <option value="Monica">Monica</option>
+          <option value="Joey">Joey</option>
+          <option value="Chandler">Chandler</option>
+          <option value="Ross">Ross</option>
+          <option value="Phoebe">Phoebe</option>
+        </select>
         <input type="submit" />
       </form>
       <div className="backButton">
