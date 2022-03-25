5fa3122514b16674ba4aa2364a911a70717ced62
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-03-24 18:24:14 -0700

    added hook to baker model

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,24 +1,34 @@
 const express = require("express");
 const breads = express.Router();
 const Bread = require("../models/bread.js");
-
-// Index
-breads.get("/", (req, res) => {
-  Bread.find().then((foundBreads) => {
-    res.render("index", {
-      breads: foundBreads,
-      title: "Index Page",
-    });
-  });
-});
+const Baker = require("../models/baker.js");
+// Index:
+breads.get('/', (req, res) => {
+  Baker.find()
+    .then(foundBakers => {
+      Bread.find()
+      .then(foundBreads => {
+          res.render('index', {
+              breads: foundBreads,
+              bakers: foundBakers,
+              title: 'Index Page'
+          })
+      })
+    })
+})
 // NEW
 breads.get("/new", (req, res) => {
-  res.render("new");
+  Baker.find().then((foundBakers) => {
+    res.render("new", {
+      bakers: foundBakers,
+    });
+  });
 });
 // SHOW
 // SHOW
 breads.get("/:id", (req, res) => {
   Bread.findById(req.params.id)
+    .populate("baker")
     .then((foundBread) => {
       const bakedBy = foundBread.getBakedBy();
       console.log(bakedBy);
@@ -60,13 +70,19 @@ breads.put("/:id", (req, res) => {
   );
 });
 // EDIT
-breads.get("/:id/edit", (req, res) => {
-  Bread.findById(req.params.id).then((foundBread) => {
-    res.render("edit", {
-      bread: foundBread,
-    });
-  });
-});
+// EDIT
+breads.get('/:id/edit', (req, res) => {
+  Baker.find()
+    .then(foundBakers => {
+        Bread.findById(req.params.id)
+          .then(foundBread => {
+            res.render('edit', {
+                bread: foundBread, 
+                bakers: foundBakers 
+            })
+          })
+    })
+})
 
 // DELETE
 breads.delete("/:id", (req, res) => {
--- models/bread.js
+++ models/bread.js
@@ -4,19 +4,18 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 // schema
-  const breadSchema = new Schema({
-    name: { type: String, required: true },
-    hasGluten: Boolean,
-    image: { type: String, default: "http://placehold.it/500x500.png" },
-    baker: {
-      type: String,
-      enum: ["Rachel", "Monica", "Joey", "Chandler", "Ross", "Phoebe"],
-    },
-  });
-
+const breadSchema = new Schema({
+  name: { type: String, required: true },
+  hasGluten: Boolean,
+  image: { type: String, default: "https://picsum.photos/seed/picsum/200/300" },
+  baker: {
+    type: Schema.Types.ObjectID,
+    ref: "Baker",
+  },
+});
   // helper methods 
 breadSchema.methods.getBakedBy = function(){
-  return `${this.name} was baked with love by ${this.baker}`
+  return `${this.name} was baked with love by ${this.baker.name}, who has been with us since ${this.baker.startDate.getFullYear()}`
 }
 
 // model and export
