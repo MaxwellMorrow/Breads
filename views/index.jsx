a6fa07dc91de3e7f7aae94ae24a443066a28d32b
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-03-29 19:13:42 -0700

    trying to fix routing for deployment

--- views/index.jsx
+++ views/index.jsx
@@ -1,5 +1,5 @@
 const React = require("react");
-const Default = require("./layouts/Default");
+const Default = require("./layouts/default.jsx");
 
 function Index({ breads, bakers, title }) {
   return (
